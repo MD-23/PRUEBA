27ab949ca19802255440d46fcfb9c966d51a4a11
MD-23 <mdieguez@itba.edu.ar>
2015-09-30 11:26:51 -0300

    prueba comentari

---
+++ tp5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char nombre[]={}
+	char opciones[5][20];
+	char parametros[4][20];
+	
+	int i;
+	for (i=0;i<argc;i++)
+	
+		if(i=0)
+		{
+			printf("El nombre del archivo es%s\n",argv[i]);
+			//nombre[]=argv[i];
+			printf("nombre %s\n",nombre[i]);
+		}
+		
+		
+	return 0;
+}
